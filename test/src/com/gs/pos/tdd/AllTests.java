6ed66c37a0a2d7e51c4ea1bf25b7a7da82ca6083
edgar.gs@gmail.com <edgar.gs@gmail.com@7f40dbc4-aceb-11dd-a714-1752b6e4c161>
2014-09-12 21:40:09 +0000

    Abstract DAOTransaccion

---
+++ test/src/com/gs/pos/tdd/AllTests.java
@@ -0,0 +1,16 @@
+package com.gs.pos.tdd;
+
+import com.gs.pos.configuracion.test.TestConfiguracion;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TestConfiguracion.class
+})
+public class AllTests {
+    public AllTests() {
+        super();
+    }
+}

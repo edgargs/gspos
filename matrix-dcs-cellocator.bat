5ad0a6ee0b5279e29f5a9d3930d6f56cac9268c7
Edgar Rios Navarro <edgar.rios@hacom.com.pe>
2017-05-24 18:47:40 -0500

    Asignacion de nivel de log

--- matrix-dcs-cellocator.bat
+++ matrix-dcs-cellocator.bat
@@ -1,4 +1,5 @@
-cd build/libs
+copy src\main\resources\configC.properties build\libs
+cd build\libs
 set logProgram=logCellocator
 set configProgram=configC.properties
-java -Dconfig.program=%configProgram% -Dlog.program=%logProgram% -jar matrix-dcs-1.1.0-SNAPSHOT.jar 30500
+java -Dconfig.program=%configProgram% -Dlog.program=%logProgram% -Dlevel.program=INFO -jar matrix-dcs-1.1.0-SNAPSHOT.jar 30400
